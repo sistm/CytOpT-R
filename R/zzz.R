836d6c25774b4a67ed0b8affc4c6ea6642a645bb
borishejblum <boris.hejblum@gmail.com>
2025-03-28 11:52:31 +0100

    updating pkgdown yaml for article run

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname) {
   reticulate::py_require("numpy")
-  reticulate::py_require("scikit-learn")
+  reticulate::py_require("sklearn")
   reticulate::py_require("scipy")
 }
